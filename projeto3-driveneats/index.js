a6b3082a0a8ab1bfaaf6a6790e5030eca507a67e
Rogerio Junior <rogerio.junior1016@gmail.com>
2022-06-13 13:52:27 -0300

    Ending project

--- projeto3-driveneats/index.js
+++ projeto3-driveneats/index.js
@@ -1,8 +1,10 @@
+/* auxiliary variables */
 let choices = 0;
 let food = 0;
 let drink = 0;
 let dessert = 0;
 
+/* Setting prices and names of the options there are selected */
 let food_price = 0;
 let drink_price = 0;
 let dessert_price = 0;
@@ -11,14 +13,10 @@ let food_name = null;
 let drink_name = null;
 let dessert_name = null;
 
-const food_option = document.querySelectorAll(".food");
-const drink_option = document.querySelectorAll(".drink");
-const dessert_option = document.querySelectorAll(".dessert");
-
-console.log(food_option);
-console.log(document.querySelector(".food"));
+/* Selection functions */
 
 function selectFood(value) {
+  const section_food = document.querySelector(".foods");
   if (food === 0) {
     const check_mark = value.querySelector(".option-footer");
     check_mark.innerHTML += '<img src="img/checkmark-circle.svg" alt="">';
@@ -26,15 +24,11 @@ function selectFood(value) {
     food = 1;
     choices += 1;
   } else {
-    for (i of food_option) {
-      console.log(i.classList.contains("click"));
-      if (i.classList.contains("click") === true) {
-        i.classList.remove("click");
-        const check_mark = i.querySelector(".option-footer img");
-        check_mark.remove();
-      }
-    }
-    const check_mark = value.querySelector(".option-footer");
+    const selected_element = section_food.querySelector(".click");
+    selected_element.classList.remove("click");
+    let check_mark = selected_element.querySelector(".option-footer img");
+    check_mark.remove();
+    check_mark = value.querySelector(".option-footer");
     check_mark.innerHTML += '<img src="img/checkmark-circle.svg" alt="">';
     value.classList.add("click");
   }
@@ -55,6 +49,7 @@ function selectFood(value) {
 }
 
 function selectDrink(value) {
+  const section_drink = document.querySelector(".drinks");
   if (drink === 0) {
     const check_mark = value.querySelector(".option-footer");
     check_mark.innerHTML += '<img src="img/checkmark-circle.svg" alt="">';
@@ -62,15 +57,11 @@ function selectDrink(value) {
     drink = 1;
     choices += 1;
   } else {
-    for (i of drink_option) {
-      console.log(i.classList.contains("click"));
-      if (i.classList.contains("click") === true) {
-        i.classList.remove("click");
-        const check_mark = i.querySelector(".option-footer img");
-        check_mark.remove();
-      }
-    }
-    const check_mark = value.querySelector(".option-footer");
+    const selected_element = section_drink.querySelector(".click");
+    selected_element.classList.remove("click");
+    let check_mark = selected_element.querySelector(".option-footer img");
+    check_mark.remove();
+    check_mark = value.querySelector(".option-footer");
     check_mark.innerHTML += '<img src="img/checkmark-circle.svg" alt="">';
     value.classList.add("click");
   }
@@ -91,6 +82,7 @@ function selectDrink(value) {
 }
 
 function selectDessert(value) {
+  const section_dessert = document.querySelector(".desserts");
   if (dessert === 0) {
     const check_mark = value.querySelector(".option-footer");
     check_mark.innerHTML += '<img src="img/checkmark-circle.svg" alt="">';
@@ -98,15 +90,11 @@ function selectDessert(value) {
     dessert = 1;
     choices += 1;
   } else {
-    for (i of dessert_option) {
-      console.log(i.classList.contains("click"));
-      if (i.classList.contains("click") === true) {
-        i.classList.remove("click");
-        const check_mark = i.querySelector(".option-footer img");
-        check_mark.remove();
-      }
-    }
-    const check_mark = value.querySelector(".option-footer");
+    const selected_element = section_dessert.querySelector(".click");
+    selected_element.classList.remove("click");
+    let check_mark = selected_element.querySelector(".option-footer img");
+    check_mark.remove();
+    check_mark = value.querySelector(".option-footer");
     check_mark.innerHTML += '<img src="img/checkmark-circle.svg" alt="">';
     value.classList.add("click");
   }
@@ -125,6 +113,8 @@ function selectDessert(value) {
     )}'class='request_ready'><button class='request_ready'>Fechar pedido</button></a>`;
   }
 }
+
+/* Generating msg */
 
 function generatemsg(food_value, drink_value, dessert_value) {
   const total = (food_value + drink_value + dessert_value).toFixed(2);
