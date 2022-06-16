2012f03d0b7d2f3abf025eb2c5b4bb80cb5e9f1b
Rogerio Junior <rogerio.junior1016@gmail.com>
2022-06-16 03:54:01 -0300

    Turn function

--- projeto4-parrotscardgame/index.js
+++ projeto4-parrotscardgame/index.js
@@ -18,9 +18,8 @@ while (cards_number < 4 || cards_number > 14 || cards_number % 2 != 0) {
 }
 
 const game_table = document.querySelector(".container");
-console.dir(game_table);
 for (let i = 0; i < cards_number; i++) {
-  game_table.innerHTML += `<div class="card" id="${i}" data-identifier="card">
+  game_table.innerHTML += `<div class="card" id="${i}" onclick="turn(this)" data-identifier="card">
             <img src="img/front.svg" data-identifier="back-face" alt="">
         </div>`;
 }
@@ -30,4 +29,19 @@ for (let i = 0; i < cards_number / 2; i++) {
   game_parrots.push(all_parrots[i]);
 }
 game_parrots.sort(comparador);
-console.dir(game_parrots);
+let clicked_cards = 0;
+const current_cards = [];
+function turn(element) {
+  clicked_cards += 1;
+  console.dir(element);
+  element.classList.add("virada");
+  element.onclick = null;
+  console.dir(element);
+  element.innerHTML = game_parrots[element.id];
+  current_cards.push(game_parrots[element.id]);
+  if (clicked_cards === 2) {
+    if (current_cards[0] === current_cards[1]) {
+      current_cards.splice(0, current_cards.length);
+    }
+  }
+}
