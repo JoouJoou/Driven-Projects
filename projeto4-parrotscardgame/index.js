4474ec1f82d9422328a2c90292cad1fe9fdf66f2
Rogerio Junior <rogerio.junior1016@gmail.com>
2022-06-17 18:16:55 -0300

    Fixing bugs - Adding timer - Ending Game

--- projeto4-parrotscardgame/index.js
+++ projeto4-parrotscardgame/index.js
@@ -1,47 +1,98 @@
 const all_parrots = [
-  `<img src="img/bobrossparrot.gif" data-identifier="front-face" alt="">`,
-  `<img src="img/explodyparrot.gif" data-identifier="front-face" alt="">`,
-  `<img src="img/fiestaparrot.gif" data-identifier="front-face" alt="">`,
-  `<img src="img/metalparrot.gif" data-identifier="front-face" alt="">`,
-  `<img src="img/revertitparrot.gif" data-identifier="front-face" alt="">`,
-  `<img src="img/tripletsparrot.gif" data-identifier="front-face" alt="">`,
-  `<img src="img/unicornparrot.gif" data-identifier="front-face" alt="">`,
+  `bobrossparrot.gif`,
+  `explodyparrot.gif`,
+  `fiestaparrot.gif`,
+  `metalparrot.gif`,
+  `revertitparrot.gif`,
+  `tripletsparrot.gif`,
+  `unicornparrot.gif`,
 ];
 function comparador() {
   return Math.random() - 0.5;
 }
-all_parrots.sort(comparador);
+let plays = 0;
+let game_parrots = [];
+let click = `turn(this)`;
+let timersec = 0;
+let timermin = 0;
+const count = document.querySelector(`header p`);
 
-let cards_number = prompt("Com quantas cartas quer jogar?");
-while (cards_number < 4 || cards_number > 14 || cards_number % 2 != 0) {
-  cards_number = prompt("Com quantas cartas quer jogar?");
-}
+start();
+function start() {
+  setInterval(() => {
+    timersec++;
+    if (timersec === 60) {
+      timersec = 0;
+      timermin++;
+    }
+    count.textContent = `${timermin < 10 ? `0${timermin}` : timermin}:${
+      timersec < 10 ? `0${timersec}` : timersec
+    }`;
+  }, 1000);
+  all_parrots.sort(comparador);
+  game_parrots = [];
+  let cards_number = prompt("Com quantas cartas quer jogar?");
+  while (cards_number < 4 || cards_number > 14 || cards_number % 2 != 0) {
+    cards_number = prompt("Com quantas cartas quer jogar?");
+  }
 
-const game_table = document.querySelector(".container");
-for (let i = 0; i < cards_number; i++) {
-  game_table.innerHTML += `<div class="card" id="${i}" onclick="turn(this)" data-identifier="card">
+  for (let i = 0; i < cards_number / 2; i++) {
+    game_parrots.push(all_parrots[i]);
+    game_parrots.push(all_parrots[i]);
+  }
+  game_parrots.sort(comparador);
+
+  const game_table = document.querySelector(".container");
+  game_table.innerHTML = ``;
+  for (let i = 0; i < cards_number; i++) {
+    game_table.innerHTML += `<div class="card" name="${game_parrots[i]}" onclick="${click}" data-identifier="card">
             <img src="img/front.svg" data-identifier="back-face" alt="">
         </div>`;
-}
-const game_parrots = [];
-for (let i = 0; i < cards_number / 2; i++) {
-  game_parrots.push(all_parrots[i]);
-  game_parrots.push(all_parrots[i]);
-}
-game_parrots.sort(comparador);
-let clicked_cards = 0;
-const current_cards = [];
-function turn(element) {
-  clicked_cards += 1;
-  console.dir(element);
-  element.classList.add("virada");
-  element.onclick = null;
-  console.dir(element);
-  element.innerHTML = game_parrots[element.id];
-  current_cards.push(game_parrots[element.id]);
-  if (clicked_cards === 2) {
-    if (current_cards[0] === current_cards[1]) {
-      current_cards.splice(0, current_cards.length);
-    }
   }
+  plays = 0;
+}
+
+function turn(element) {
+  plays++;
+  const flipped_card = document.querySelector(".clicked");
+  console.log(flipped_card);
+  element.classList.add("virada");
+  element.classList.add("clicked");
+  const img = element.firstElementChild;
+  img.src = `img/${element.getAttribute(`name`)}`;
+  element.setAttribute(`onclick`, ``);
+  if (flipped_card !== null) {
+    const all_cards = document.querySelectorAll(`.card`);
+    all_cards.forEach((value) => value.setAttribute(`onclick`, ``));
+    setTimeout(() => {
+      check(element, flipped_card, all_cards);
+    }, 1000);
+  }
+}
+
+function check(element1, element2, cards) {
+  if (element1.getAttribute(`name`) === element2.getAttribute(`name`)) {
+    game_parrots = game_parrots.filter((value) => {
+      return element1.getAttribute(`name`) !== value;
+    });
+    if (game_parrots.length === 0) {
+      alert(`Você ganhou em ${plays} jogadas!`);
+      const restart = prompt(`Gostaria de reiniciar a partida? (sim ou não)`);
+      if (restart === `sim`) {
+        return start();
+      }
+    }
+  } else {
+    const img1 = element1.firstElementChild;
+    const img2 = element2.firstElementChild;
+    element1.setAttribute(`onclick`, `turn(this)`);
+    element1.classList.remove("virada");
+    element2.setAttribute(`onclick`, `turn(this)`);
+    element2.classList.remove("virada");
+    img1.src = `img/front.svg`;
+    img2.src = `img/front.svg`;
+  }
+  cards.forEach((value) => value.setAttribute(`onclick`, `${click}`));
+  element1.classList.remove("clicked");
+  element2.classList.remove("clicked");
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+const a = [1, 2, 3, 4, 5];
+a.splice(0, a.length);
+console.log(a);
